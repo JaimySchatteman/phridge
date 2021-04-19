130e54dc348cbaf718c2c39ca1e647240189fd83
Jaimy <schattemanjaimy@outlook.com>
2021-04-19 08:46:23 +0200

    🚧 Testing endpoinst from frontend

--- api/api.py
+++ api/api.py
@@ -3,13 +3,14 @@ from clarifai_grpc.channel.clarifai_channel import ClarifaiChannel
 from clarifai_grpc.grpc.api import resources_pb2, service_pb2, service_pb2_grpc
 from clarifai_grpc.grpc.api.status import status_pb2, status_code_pb2
 from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
 
 channel = ClarifaiChannel.get_grpc_channel()
 stub = service_pb2_grpc.V2Stub(channel)
 metadata = (('authorization', 'Key 6c48b3b707844ce0898a00ef7ff57ee1'),)
 
 app = Flask(__name__)
-
+CORS(app)
 
 class Prediction:
     def __init__(self, name, score):
@@ -20,8 +21,14 @@ class Prediction:
         return {"name": self.name, "score": self.score}
 
 
+@app.route('/api/test', methods=['GET'])
+def test():
+    return 'test'
+
 @app.route('/api/search-ingredients', methods=['POST'])
 def search():
+    img = Image.open(request.files['image'])
+    print(img)
     response = stub.PostModelOutputs(
         service_pb2.PostModelOutputsRequest(
             model_id="bd367be194cf45149e75f01d59f77ba7",
